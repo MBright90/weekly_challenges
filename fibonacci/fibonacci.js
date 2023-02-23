56cb0dc6c84fbf897b9c72a506a7317301b2fc8e
mbright90 <michael.bright90@icloud.com>
2023-02-23 18:38:44 +0000

    Upload fibonacci function solution

---
+++ fibonacci/fibonacci.js
@@ -0,0 +1,27 @@
+const fibonacciEquation = (length, current = 1, prev = 0, arr = []) => {
+  // If length is less than required to show a fibonacci sequence,
+  // return -1
+
+  // If recursive iterations have reached the length desired by user, return the
+  // current value
+	if (length === 0) return current
+  
+  if (current > Number.MAX_SAFE_INTEGER) [current, prev] = [BigInt(current), BigInt(prev)]
+  
+  // Recursive case  
+  fibonacciEquation(length - 1, current + prev, current, arr)
+  arr.unshift(current.toString())
+  
+  return arr
+}
+
+// Wrap fibonacci to check whether length is too short
+// and to return a 0 as the beginning value
+const fibonacci = (length) => {
+  if (length <= 1) return -1
+	const fibResult = fibonacciEquation(length)
+  fibResult.unshift("0")
+  return fibResult
+}
+
+console.log(fibonacci(100).join(', '))
